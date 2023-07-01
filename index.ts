669aeb4f827cfa5e37a5cbb9939e9c49478b4fdf
JuhyunjaceLee <heyjace777@gmail.com>
2023-07-01 14:43:07 +0900

    study about type

---
+++ index.ts
@@ -0,0 +1,21 @@
+// number
+// string
+// boolean
+// null
+// undefined
+// any
+
+let a: number = 3;
+// a = "abc"
+a = 10;
+
+let b: string = "hello";
+// b = true;
+b = "hi";
+
+let c: any = 10;
+c = "hello";
+
+let d: number | string = "hello";
+d = 100;
+// d = false;
